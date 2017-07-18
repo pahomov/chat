20228f2fab98c1270fa262a822d9b9570e2b9f9c
Ivan Pahomov <ivan.pahomov@iqoption.com>
2017-07-18 21:13:21 +0300

    init

---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const request = require('request');
+const should = require('should');
+
+describe('section descr', function() {
+  describe('dummy test', function() {
+    it('always true', function(done) {
+      done();
+    });
+  });
+});
